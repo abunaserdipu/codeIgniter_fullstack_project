e72e3acedda15c0571eccc50ecf05d67ddc309d9
abunaserdipu <abunaserdipu0@gmail.com>
2023-01-04 18:58:48 +0600

    multiple query-builder on wdpf51_sampledb database

--- backend/app/Controllers/QueryBuilder.php
+++ backend/app/Controllers/QueryBuilder.php
@@ -8,14 +8,14 @@ class QueryBuilder extends BaseController
 {
     public function index()
     {
-        $db      = \Config\Database::connect();
-        $builder = $db->table('products');
-        // $builder = $db->table('users');
-        // echo "<pre>";
-        $query = $builder->get();
-        $data['raw'] = $query->getResult();
-        // print_r($row);
-        return view('products/test', $data);
+        // $db      = \Config\Database::connect();
+        // $builder = $db->table('products');
+        // // $builder = $db->table('users');
+        // // echo "<pre>";
+        // $query = $builder->get();
+        // $data['raw'] = $query->getResult();
+        // // print_r($row);
+        // return view('products/test', $data);
 
         // $builder = $db->table('products');
         // $builder->select('id ,product_name, product_details, product_price');
@@ -55,6 +55,69 @@ class QueryBuilder extends BaseController
         // echo "<pre>";
         // print_r($query);
 
+        // $db      = \Config\Database::connect('another');
+        // // $otherdb = $this->load->database('wdpf51_sampledb', TRUE); // the TRUE paramater tells CI that you'd like to return the database object.
+        // $builder = $db->table('products')->select('name, details');
+        // $query = $builder->get()->getResult();
+        // echo "<pre>";
+        // print_r($query);
 
+        // **************** 1
+        // $db = \Config\Database::connect('another');
+        // $builder = $db->table('employees')->select('firstName,lastName,email,jobTitle')->where('jobTitle = "Sales Rep"');
+        // $query = $builder->get()->getResultArray();
+        // echo "<pre>";
+        // print_r($query);
+
+        // **************** 2
+        // $db = \Config\Database::connect('another');
+        // $builder = $db->table('employees')->select('firstName,lastName,email,jobTitle')->where('jobTitle = "Sales Rep" and reportsTo = 1143');
+        // $builder->select('firstName as ReportsTo')->where('employee');
+        // $query = $builder->get()->getResult();
+        // echo "<pre>";
+        // print_r($query);
+
+        // **************** 3
+        // $db = \Config\Database::connect('another');
+        // $builder = $db->table('employees,offices')->select('firstName,lastName,email,city,country')->where('employees.officeCode = offices.officeCode and offices.country = "USA"');
+        // $query = $builder->get()->getResult();
+        // echo "<pre>";
+        // print_r($query);
+
+        // **************** 4
+        // $db = \Config\Database::connect('another');
+        // $builder = $db->table('orders,customers')->select('customerName,phone,city,orderNumber,orderDate,status')->where('orders.customerNumber = customers.customerNumber');
+        // $query = $builder->get()->getResult();
+        // echo "<pre>";
+        // print_r($query);
+
+        // **************** 5
+        // $db = \Config\Database::connect('another');
+        // $builder = $db->table('orders,orderdetails,customers')->select('customerName,phone,city,orders.orderNumber,orderDate,status,quantityOrdered,priceEach')->where('orders.customerNumber = customers.customerNumber and orderdetails.orderNumber = orders.orderNumber');
+        // $query = $builder->get()->getResult();
+        // echo "<pre>";
+        // print_r($query);
+
+        // **************** 6
+
+        // $db = \Config\Database::connect('another');
+        // $builder = $db->table('orders, orderdetails, customers, products')->select('customerName, city, orders.orderNumber, orderDate, products.productCode, productName, quantityOrdered, priceEach, MSRP')->selectSum('priceEach')->selectSum('quantityOrdered')->groupBy('orders.orderNumber')->where('orders.customerNumber = customers.customerNumber and orderdetails.orderNumber = orders.orderNumber and orderdetails.productCode = products.productCode');
+        // $query = $builder->get()->getResult();
+        // echo "<pre>";
+        // print_r($query);
+
+        // **************** 7
+        // $db = \Config\Database::connect('another');
+        // $builder = $db->table('orders,customers')->select('status,country')->where('orders.customerNumber = customers.customerNumber');
+        // $query = $builder->get()->getResult();
+        // echo "<pre>";
+        // print_r($query);
+
+        // **************** 8
+        $db = \Config\Database::connect('another');
+        $builder = $db->table('orders,customers')->select('status,customerName')->where('orders.customerNumber = customers.customerNumber');
+        $query = $builder->get()->getResult();
+        echo "<pre>";
+        print_r($query);
     }
 }
